5ee68a7224b7399add0ff3b3eb5a60090f8dbf6c
elara <2895004094@qq.com>
2026-04-22 11:29:20 +0800

    用户签到功能与统计连续签到天数

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -12,14 +12,19 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -36,7 +41,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Override
     public Result sendCode(String phone, HttpSession session) {
 //        1、校验手机号
-        if (RegexUtils.isPhoneInvalid(phone)){
+        if (RegexUtils.isPhoneInvalid(phone)) {
             //        2、手机号格式有误
             return Result.fail("手机号格式有误");
         }
@@ -54,13 +59,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     public Result login(LoginFormDTO loginForm, HttpSession session) {
 //        1、校验手机号
         String phone = loginForm.getPhone();
-        if (RegexUtils.isPhoneInvalid(phone)){
+        if (RegexUtils.isPhoneInvalid(phone)) {
             //        2、手机号格式有误
             return Result.fail("手机号格式有误");
         }
 //        3、从redis中获取验证码并校验
         String CacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
-        if (CacheCode == null || !CacheCode.equals(loginForm.getCode())){
+        if (CacheCode == null || !CacheCode.equals(loginForm.getCode())) {
 //            4、验证码格式有误
             return Result.fail("验证码格式有误");
         }
@@ -89,6 +94,64 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
 //        8、返回token
         return Result.ok(token);
+    }
+
+    @Override
+    public Result sign() {
+//        1、获取用户信息
+        Long userId = UserHolder.getUser().getId();
+//        2、获取日期
+        LocalDateTime now = LocalDateTime.now();
+//        3、拼接用户信息与日期
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+//        4、获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+//        5、存入redis
+        stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
+        return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+//        1、获取用户信息
+        Long userId = UserHolder.getUser().getId();
+//        2、获取日期
+        LocalDateTime now = LocalDateTime.now();
+//        3、拼接用户信息与日期
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+//        4、获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+//        5、获取本月截至今天为止的所有签到记录，返回的是十进制数字
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+        if (result == null || result.isEmpty()) {
+            // 没有签到记录
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        if (num == 0) {
+            return Result.ok(0);
+        }
+//        6、循环遍历
+        int count = 0;
+        while (true) {
+//            6.1、让这个数字与1做与运算，得到数字的最后一个bit位并判断bit是否为0
+            if ((num & 1) == 0) {
+//                6.2、为0，未签到，结束
+                break;
+            } else {
+//                6.3、不为0，签到，计数器+1
+                count++;
+            }
+//            6.4、数字右移，继续判断下一个bit
+            num >>>= 1;
+        }
+        return Result.ok(count);
     }
 
     private User CreateUserWithPhone(String phone) {
